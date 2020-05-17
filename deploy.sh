58a6643efe43407dfbf49882e69cfe68ef1c0eab
João Paulo Novais <joaopaulonovais@gmail.com>
2020-05-17 08:16:28 -0300

    :arrow_up: configuration updated

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,12 @@
 echo " `date` : start "
 
-echo " Change to the project directory "
-# cd domains/brinquecoin.com/public_html/app
-
 echo " Turn on maintenance mode "
 # php artisan down
-php artisan down --message="Updating app" --retry=60
+php artisan down --message="Updating site" --retry=60
 
 echo " Pull the latest changes from the git repository "
-git reset --hard
-git clean -df
+# git reset --hard
+# git clean -df
 git pull origin master
 
 echo " Install/update composer dependecies "
@@ -35,16 +32,6 @@ php artisan config:cache
 echo " Clear and cache views "
 php artisan view:clear
 php artisan view:cache
-
-# echo " Install node modules "
-# npm install
-
-# echo " Build assets using Laravel Mix "
-# npm run production
-# npm ci
-
-# echo " Generate SW "
-# npm run generateSW
 
 echo " Turn off maintenance mode "
 php artisan up
